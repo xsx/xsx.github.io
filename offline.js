b57308eb491548ba2aa679add232354b30407598
xsx <xsx.dog@gmail.com>
2020-05-17 21:49:37 +0200

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,1 @@
+{"version":1589744834118,"fileList":["c2runtime.js","data.js","start.js","style.css","offlineclient.js","images/shared-0-sheet0.png","images/shared-0-sheet1.png","images/shared-0-sheet2.png","icons/icon-16.png","icons/icon-32.png","icons/icon-114.png","icons/icon-128.png","icons/icon-256.png","icons/loading-logo.png","register-sw.js"]}
